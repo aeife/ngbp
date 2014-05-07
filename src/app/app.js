b7bcb02fc2b13282667fc5588bb1ac5453811178
Andre Eife <ande@spreadshirt.net>
2014-05-07 16:53:24 +0200

    centralized routing

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,27 @@ angular.module( 'ngBoilerplate', [
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $stateProvider
+    .state( 'home', {
+      url: '/home',
+      views: {
+        "main": {
+          controller: 'HomeCtrl',
+          templateUrl: 'home/home.tpl.html'
+        }
+      },
+      data:{ pageTitle: 'Home' }
+    })
+    .state( 'about', {
+      url: '/about',
+      views: {
+        "main": {
+          controller: 'AboutCtrl',
+          templateUrl: 'about/about.tpl.html'
+        }
+      },
+      data:{ pageTitle: 'What is It?' }
+    });
   $urlRouterProvider.otherwise( '/home' );
 })
 
